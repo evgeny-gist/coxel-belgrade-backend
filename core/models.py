d12a8e71abb7159a906b9ee38a2c66ef6acf9dae
rerf <rerf2010rerf@gmail.com>
2023-11-25 18:44:41 +0100

    models.py

--- core/models.py
+++ core/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
 # Create your models here.
+class Case(models.Model):
+    class CaseType(models.IntegerChoices):
+        COMPLETE = 1
+        INTERMEDIATE_RECOMMENDATION = 2
+        INTERMEDIATE_QUESTION = 3
+
+    recommendation = models.TextField()
+    type = models.IntegerField(choices=CaseType.choices)
+
+
+class Attr(models.Model):
+    priority = models.IntegerField()
+    name = models.CharField(max_length=255)
+    question = models.TextField()
+
+
+class AttrValue(models.Model):
+    case_id = models.ForeignKey(Case, on_delete=models.CASCADE)
+    attr_id = models.ForeignKey(Attr, on_delete=models.CASCADE)
+    value = models.CharField(255)
