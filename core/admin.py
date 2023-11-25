9f6fd2d6e9a2d790e511d4826b61619407596fd8
Evgeny <thezhenikuls@gmail.com>
2023-11-25 19:28:30 +0100

    make admin

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,22 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import *
+
+
+class CaseAdmin(admin.ModelAdmin):
+    list_display = ["name", "update_date"]
+
+    fieldsets = [
+        ("Basic",
+         {"fields": ['name',
+                     'recommendation',
+                     'type', ]}),
+        ("Date information",
+         {"fields": ['create_date',
+                     'update_date', ]}),
+    ]
+
+
+admin.site.register(Case, CaseAdmin)
+admin.site.register(Attr)
+admin.site.register(AttrValue)
--- core/models.py
+++ core/models.py
@@ -1,4 +1,6 @@
 from django.db import models
+from datetime import datetime
+
 
 # Create your models here.
 class Case(models.Model):
@@ -7,17 +9,26 @@ class Case(models.Model):
         INTERMEDIATE_RECOMMENDATION = 2
         INTERMEDIATE_QUESTION = 3
 
-    recommendation = models.TextField()
+    name = models.CharField(max_length=255)
+    recommendation = models.TextField(blank=True)
     type = models.IntegerField(choices=CaseType.choices)
+    create_date = models.DateTimeField("Created on", blank=True)
+    update_date = models.DateTimeField("Updated on", blank=True)
+
+    def __str__(self):
+        return self.name
 
 
 class Attr(models.Model):
     priority = models.IntegerField()
     name = models.CharField(max_length=255)
-    question = models.TextField()
+    question = models.TextField(blank=True)
+
+    def __str__(self):
+        return self.name
 
 
 class AttrValue(models.Model):
     case_id = models.ForeignKey(Case, on_delete=models.CASCADE)
     attr_id = models.ForeignKey(Attr, on_delete=models.CASCADE)
-    value = models.CharField(255)
+    value = models.CharField(max_length=255, blank=True)
